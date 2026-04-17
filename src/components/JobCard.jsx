d078f183bb5ba855831869753b86932d10439dab
fguedez1311 <46085315+fguedez1311@users.noreply.github.com>
2026-04-17 07:19:09 -0400

    feat:Se inhabilitar los botones de los registro de trabajo con la sesión

--- src/components/JobCard.jsx
+++ src/components/JobCard.jsx
@@ -1,29 +1,48 @@
 import { useState } from "react";
 import { Link } from "./Link";
 import styles from "./JobCard.module.css";
+import { useAuthStore } from "../store/authStore";
 import { useFavoritesStore } from "../store/favoritesStore";
+function JobCardFavoriteButton({ jobId }) {
+  const { isLoggedIn } = useAuthStore();
+  // suscríbete a TODA la store y extra TODA la store
+  const { toggleFavorite, isFavorite } = useFavoritesStore();
 
-function JobCardFavoriteButton({jobId}){
-   const {toggleFavorite,isFavorite}=useFavoritesStore()
-  
-   return(
-      <button onClick={()=>toggleFavorite(jobId)}>
-              {isFavorite(jobId) ? '❤️': '❤'}
-      </button>
-   )
+  return (
+    <button 
+      disabled={!isLoggedIn}
+      onClick={() => toggleFavorite(jobId)}
+       aria-label={isFavorite(jobId) ? 'Remove from favorites' : 'Add to favorites'}
+      
+    >
+      {isFavorite(jobId) ? "❤️" : "❤"}
+    </button>
+  );
+}
+function JobCardApplyButton({ jobId }) {
+  const [isApplied, setIsApplied] = useState(false);
+  const { isLoggedIn } = useAuthStore();
+  const text = isApplied ? "Aplicado" : "Aplicar";
+  const buttonClass = isApplied ? "isApplied" : "";
+
+  const handleApplyClick = () => {
+    console.log("Aplicando al trabajo con id:", jobId);
+    setIsApplied(true);
+  };
+  return (
+    <button
+      disabled={!isLoggedIn}
+      onClick={handleApplyClick}
+      className={`boton-azul ${buttonClass} `}
+    >
+      {text}
+    </button>
+  );
 }
 
 export function JobCard({ job }) {
   const { id, data, titulo, empresa, ubicacion, descripcion } = job;
-  const [isApplied, setIsApplied] = useState(false);
- 
 
-  function handleClick() {
-    setIsApplied(!isApplied);
-  }
-  const text = isApplied ? "Aplicado" : "Aplicar";
-  const buttonClass = isApplied ? "isApplied" : "";
-  const isAppliedText = isApplied ? "Si" : "No";
   return (
     <article
       className="resultados__article"
@@ -38,18 +57,16 @@ export function JobCard({ job }) {
           </Link>
         </h3>
         <small className="resultados__small">
-          {empresa} - {ubicacion} -¿He aplicado? {isAppliedText}
+          {empresa} - {ubicacion}
         </small>
         <p className="resultados__p">{descripcion}</p>
       </div>
       <div className="styles.actions">
-          <Link  href={`/jobs/${id}`} className={styles.details}>
-                Ver detalles
-          </Link>
-          <button onClick={handleClick} className={`boton-azul ${buttonClass} `}>
-            {text}
-          </button>
-          <JobCardFavoriteButton jobId={job.id}/>
+        <Link href={`/jobs/${id}`} className={styles.details}>
+          Ver detalles
+        </Link>
+        <JobCardApplyButton jobId={job.id} />
+        <JobCardFavoriteButton jobId={job.id} />
       </div>
     </article>
   );
